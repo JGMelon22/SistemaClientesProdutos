3923930297517fa9f3a153fe358b902fa86e1b01
JGMelon22 <jgvasconcellos22@gmail.com>
2023-05-04 13:11:37 -0300

    Desenvolvendo a View Index para ClientesProdutos

--- DbStructure.sql
+++ DbStructure.sql
@@ -1,20 +1,20 @@
 -- Table clients
 CREATE TABLE clients
 (
-    id        INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-    client_name      VARCHAR(50) NOT NULL,
-    last_name VARCHAR(50) NOT NULL,
-    email     VARCHAR(50) NOT NULL,
-    active    NUMBER      NOT NULL -- To use boolean FROM ASP NET
+    id          INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    client_name VARCHAR(50) NOT NULL,
+    last_name   VARCHAR(50) NOT NULL,
+    email       VARCHAR(50) NOT NULL,
+    active      NUMBER      NOT NULL -- To use boolean FROM ASP NET
 );
 
 -- Table products
 CREATE TABLE products
 (
-    id     INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-    product_name   VARCHAR(100) NOT NULL,
-    value  FLOAT        NOT NULL,
-    active NUMBER       NOT NULL -- To use boolean FROM ASP NET
+    id           INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    product_name VARCHAR(100) NOT NULL,
+    value        FLOAT        NOT NULL,
+    active       NUMBER       NOT NULL -- To use boolean FROM ASP NET
 );
 
 -- Table clients_products
