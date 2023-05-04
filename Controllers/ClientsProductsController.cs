93f089c45cef9ff288fc04751ceac4ce4cd69cea
JGMelon22 <jgvasconcellos22@gmail.com>
2023-05-04 16:38:39 -0300

    Modificando a View Index de "ProductClient" para receber uma ViewBag de "NameSortParam" e modificando sua controller para incluir esse filtro

--- Controllers/ClientsProductsController.cs
+++ Controllers/ClientsProductsController.cs
@@ -1,23 +1,28 @@
 using ClientesProdutos.Infrastructure.Repositories;
+using ClientesProdutos.Services;
+using ClientesProdutos.ViewModels.ClientProduct;
 
 namespace ClientesProdutos.Controllers;
 
 public class ClientsProductsController : Controller
 {
-    private readonly IDbConnection _dbConnection;
+    // private readonly IDbConnection _dbConnection;
+    private readonly SortingService<GetClientProductViewModel> _sortingClientProduct;
     private readonly IMapper _mapper;
 
-    public ClientsProductsController(IDbConnection dbConnection, IMapper mapper)
+    public ClientsProductsController(SortingService<GetClientProductViewModel> sortingClientProduct, IMapper mapper)
     {
-        _dbConnection = dbConnection;
+        _sortingClientProduct = sortingClientProduct;
         _mapper = mapper;
     }
 
     [HttpGet]
-    public async Task<IActionResult> Index()
+    public async Task<IActionResult> Index(string sortOrder)
     {
-        var clientProductRepository = new ClientProductRepository(_dbConnection, _mapper);
-        var clientsProducts = await clientProductRepository.GetClientProductsRelation();
+        // var clientProductRepository = new ClientProductRepository(_dbConnection, _mapper);
+        ViewBag.NameSortParam = string.IsNullOrEmpty(sortOrder) ? "name_desc" : "";
+
+        var clientsProducts = await _sortingClientProduct.SortModel(sortOrder);
         return clientsProducts != null
             ? await Task.Run(() => View(clientsProducts))
             : NoContent();
